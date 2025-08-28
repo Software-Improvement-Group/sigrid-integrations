a6f8b14ad71b93360d5e57c3a959fd3aff0b50f3
Marijn Dessens <m.dessens@sig.eu>
2025-08-28 17:21:25 +0200

    Add script to integrations and Dockerfile

--- integrations.sh
+++ integrations.sh
@@ -26,7 +26,16 @@ usage() {
     exit 1
 }
 
-COMMANDS=("report-generator" "objectives_report.py" "get_scope_file.py" "export_portfolio_dependencies.py", "polarion-integration.py", "export_github_issues.py", "export_gitlab_issues.py")
+COMMANDS=(
+    "report-generator"
+    "objectives_report.py"
+    "get_scope_file.py"
+    "export_portfolio_dependencies.py"
+    "polarion-integration.py"
+    "export_github_issues.py"
+    "export_gitlab_issues.py"
+    "generate-scope-file-inventory.py"
+)
 
 if [ $# -lt 2 ] || [ -z $SIGRID_CI_TOKEN ]; then
     usage
